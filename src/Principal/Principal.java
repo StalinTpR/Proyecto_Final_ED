d6b0340b323f06c3e5eb4da2f2b6f6b37db4409e
StalinTpR <75907585+StalinTpR@users.noreply.github.com>
2020-12-13 22:16:29 -0500

    Stalin
    practica

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -12,5 +12,6 @@ package Principal;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Stalin");
     }
 }
