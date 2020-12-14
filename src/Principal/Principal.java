b48a4b88dcd7a3377f1fabdb2514c20ca7a64a34
Stali <Stali@DESKTOP-7FFNGTJ>
2020-12-13 21:38:46 -0500

    Prueba Hola

---
+++ src/Principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author Stalin Jimenez
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
