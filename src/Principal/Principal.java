b64ec32591ccdbd895689e106c6fc393564364ae
Aoko <67608060+Aoko-uwu@users.noreply.github.com>
2020-12-13 22:17:20 -0500

    Update Principal.java

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -13,5 +13,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Stalin");
+        System.out.println("ASD");
     }
 }
